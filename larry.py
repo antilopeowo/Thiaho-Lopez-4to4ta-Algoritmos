0271b484990da869b4fa172982a15b506d9707be
Amir <thiagolopezagustin@gmail.com>
2025-10-16 13:20:13 -0300

    teto.py

--- larry.py
+++ larry.py
@@ -2,7 +2,9 @@ import random
 suma=0
 clima=[]
 aux=0
+aux2=0
 indice=0
+indice2=0
 for x in range(1, 31):
     temperatura = random.randint(0, 40)
     clima.append(temperatura)
@@ -12,8 +14,8 @@ for t in range(0, len(clima)):
         indice = t+1
         aux=clima[t]
     if clima[t] < aux:
-        indice = t + 1
-        aux = clima[t]
+        indice2 = t + 1
+        aux2 = clima[t]
     if clima[t]>=26:
         print(f"hace calor.{clima[t]}")
     elif clima[t]<=16:
@@ -21,5 +23,6 @@ for t in range(0, len(clima)):
     else:
         print(f"esta estable.{clima[t]}")
 promedio=suma/len(clima)
-print(f"el dia con mayor temperatura es el:{indice} con {aux}")
+print(f"el dia con mayor temperatura es el:{indice} con {aux} y el mas frio es el:{indice2} con {aux2}")
 print(promedio)
+
